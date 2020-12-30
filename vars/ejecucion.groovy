de38e4f8fa783bea6e0b5f09decbf4e6fecd5fe1
Cristian Tapia <criss.aquiles@gmail.com>
2020-12-30 16:55:44 -0300

    subida de archivo ejecucion.groovy error en llave

--- vars/ejecucion.groovy
+++ vars/ejecucion.groovy
@@ -1,27 +1,27 @@
 def call(){
 
-pipeline {
-    agent any
-    parameters { choice(name: 'compilador', choices:['gradle','maven'], description:'compilador de construcion para aplicacion')}
-    stages{
-        stage('pipeline'){
-            steps{
-                script{
-                    echo 'herramienta seleccionada: ' + params.compilador
-                    /*"${params.compilador}".call()*/
-                    /*def pipe = load "${params.compilador}.groovy"
-                    pipe.call()*/
-                    if(params.compilador == 'gradle'){
-                        gradle.call()
-                    }else {
-                        maven.call()
+    pipeline {
+        agent any
+        parameters { choice(name: 'compilador', choices:['gradle','maven'], description:'compilador de construcion para aplicacion')}
+        stages{
+            stage('pipeline'){
+                steps{
+                    script{
+                        echo 'herramienta seleccionada: ' + params.compilador
+                        /*"${params.compilador}".call()*/
+                        /*def pipe = load "${params.compilador}.groovy"
+                        pipe.call()*/
+                        if(params.compilador == 'gradle'){
+                            gradle.call()
+                        }else {
+                            maven.call()
 
+                        }
                     }
                 }
             }
         }
+
     }
-
 }
-
 return this;
