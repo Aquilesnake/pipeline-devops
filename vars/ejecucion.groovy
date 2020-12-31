a5a9014518a090b4e86d3d1d439d4c2c53cacd15
Cristian Tapia <criss.aquiles@gmail.com>
2020-12-31 11:24:28 -0300

    se actualizan params en jenkins y en ejecucion.groovy4 elseif

--- vars/ejecucion.groovy
+++ vars/ejecucion.groovy
@@ -15,7 +15,7 @@ def call(){
                         pipe.call()*/
                         if(params.stage == 'gradle' && params.sub_stage == 'build'){
                             gradle.call('build & test')
-                            }if else{params.stage == 'gradle' && params.sub_stage == 'build,test y run'
+                            }else if{params.stage == 'gradle' && params.sub_stage == 'build,test y run'
                                 gradle.call('build & test')
                                 gradle.call('run')
                                     }else{params.compilador == 'gradle' && params.sub_stage == 'fullbuild'
@@ -23,7 +23,7 @@ def call(){
                                     }               
                         if(params.stage == 'maven' && params.sub_stage == 'build'){
                             gradle.call('build & test')
-                            }  if else {params.stage == 'maven' && params.sub_stage == 'build,test y run'
+                            }else if{params.stage == 'maven' && params.sub_stage == 'build,test y run'
                                 gradle.call('build & test')
                                 gradle.call('run')
                                     }else {params.compilador == 'maven' && params.sub_stage == 'fullbuild'
