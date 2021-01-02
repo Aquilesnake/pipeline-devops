ffea9ae25359fbb1a58dfd84c6448617cc8688bb
Cristian Tapia <criss.aquiles@gmail.com>
2021-01-02 14:07:41 -0300

    ejecucion params en stage 2

--- vars/stage.groovy
+++ vars/stage.groovy
@@ -1,5 +1,51 @@
+//stage.groovy
+
+import ejecucion.groovy.*
+
+def call(String stage.params, String sub_stage.params){
+
+    println 'Ejecución de Pipeline'
 
     pipeline{
+        agent any
+        stages{
+            stage('Pipeline'){
+                steps{
+                    script{
+                        try {
+
+                            def funciones   = new Funciones()
+
+                            stage('Inicio'){
+                                println 'Inicio'
+                                println 'String 1: ' + param1
+                                println 'String 2: ' + param2
+
+                                gradle.call()
+                            }
+
+                            stage('Union'){
+                                println 'Union de 2 Strings: ' + funciones.unirDosStrings(param1, param2)          
+                            }
+
+                            stage('MostrarNombre'){
+                                println 'Nombre obtenido desde Json: ' + funciones.mostrarNombre()
+                            }
+
+                        } catch(Exception e) {
+                            error ('Ha ocurrido el siguiente error: ' + e)
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+return this;
+
+
+   /* pipeline{
         agent any
                 stages{
             stage('pipeline'){
@@ -9,7 +55,7 @@
                         /*"${params.compilador}".call()*/
                         /*def pipe = load "${params.compilador}.groovy"
                         pipe.call()*/
-                        if(params.stage == 'gradle' && params.sub_stage == 'build'){
+                      /*  if(params.stage == 'gradle' && params.sub_stage == 'build'){
                             gradle.call('build & test')
                             }else if(params.stage == 'gradle' && params.sub_stage == 'build,test y run'){
                                 gradle.call('build & test')
@@ -32,4 +78,4 @@
         }
     }
 
-return this;
+return this;*/
