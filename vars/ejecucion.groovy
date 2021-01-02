bb182e6a83a136975509b8ea328f14e2ff14be37
Cristian Tapia <criss.aquiles@gmail.com>
2021-01-02 12:40:27 -0300

    Actualizan en ejecucion string 2

--- vars/ejecucion.groovy
+++ vars/ejecucion.groovy
@@ -3,10 +3,9 @@ def call(){
     /* groovylint-disable-next-line NglParseError */
     pipeline {
         agent any
-        parameters { choice(name: 'stage', choices:['gradle','maven'], description:'compilador de construcion')}
-        string { name: 'sub_stage', defaultValue:'', description:'Selección de stage.
-        Opciones para Gradle: Build; Sonar; Run; Test; Nexus; gitCreateRelease.
-        Opciones para Maven: Compile; Unit; Jar; Sonar; Sonar; Test; gitCreateRelease')}
+        parameters { choice(name: 'stage', choices:['gradle','maven'], description:'compilador de construcion')
+        /* groovylint-disable-next-line LineLength */
+        string (name: 'sub_stage', defaultValue:'', description:'Selección de stage')}
         /* groovylint-disable-next-line LineLength */
         /*parameters { choice(name: 'sub_stage', choices:['build','build,test y run','fullbuild'], description:'Construccion por stages')}*/
 
