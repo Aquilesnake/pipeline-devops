cc1757478e37a2ac0da63bff3adf6ec4356f8b11
Cristian Tapia <criss.aquiles@gmail.com>
2021-01-04 15:06:31 -0300

    fix code 1  ejecucion.groovy v6

--- vars/gradle.groovy
+++ vars/gradle.groovy
@@ -7,24 +7,33 @@ ejecucion.call()
 */
 
 def call (String etapa){
+
+    if(etapa == 'build'){
         stage('build'){
             bat './gradlew clean build'
-    }
-    stage('sonar') {
-         def scannerHome = tool 'sonar';
-             withSonarQubeEnv('sonar') {
-                bat "${scannerHome}\\bin\\sonar-scanner -Dsonar.projectKey=ejemplo-gradle -Dsonar.java.binaries=build"
-             }
-    }
-        stage('run') {
-            bat 'start gradlew bootRun &'
-            sleep 20
         }
-            stage('test') {
-            bat 'curl -X GET http://localhost:8082/rest/mscovid/test?msg=testing'
-            }
-                stage('nexus') {
-                nexusPublisher nexusInstanceId: 'nexus', nexusRepositoryId: 'test-nexus', packages: [[$class: 'MavenPackage', mavenAssetList: [[classifier: '', extension: 'jar', filePath: 'build\\libs\\DevOpsUsach2020-0.0.1.jar']], mavenCoordinate: [artifactId: 'DevOpsUsach2020', groupId: 'com.devopsusach2020', packaging: 'jar', version: '0.0.1']]]
+        }else if("build;sonar"){
+            stage('sonar') {
+            def scannerHome = tool 'sonar';
+                withSonarQubeEnv('sonar') {
+                    bat "${scannerHome}\\bin\\sonar-scanner -Dsonar.projectKey=ejemplo-gradle -Dsonar.java.binaries=build"
                 }
+            }
+        }
+            }else if("build;sonar:run"){
+                stage('run') {
+                    bat 'start gradlew bootRun &'
+                    sleep 20
+                }
+            }
+                }else if("build;sonar;run;test"){
+                stage('test') {
+                bat 'curl -X GET http://localhost:8082/rest/mscovid/test?msg=testing'
+                }
+                    }else if("build;sonar;run;test;nexus"){
+                    stage('nexus') {
+                    nexusPublisher nexusInstanceId: 'nexus', nexusRepositoryId: 'test-nexus', packages: [[$class: 'MavenPackage', mavenAssetList: [[classifier: '', extension: 'jar', filePath: 'build\\libs\\DevOpsUsach2020-0.0.1.jar']], mavenCoordinate: [artifactId: 'DevOpsUsach2020', groupId: 'com.devopsusach2020', packaging: 'jar', version: '0.0.1']]]
+                    }
+                     }else(""){
 }
 return this;
