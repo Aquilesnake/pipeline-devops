b1189bfe12297544277a84a7547b9c33817b5b04
Cristian Tapia <criss.aquiles@gmail.com>
2021-01-04 15:34:11 -0300

    fix code 1  ejecucion.groovy v6

--- vars/gradle.groovy
+++ vars/gradle.groovy
@@ -53,5 +53,5 @@ def call (String etapa){
                 stage('nexus') {
                 nexusPublisher nexusInstanceId: 'nexus', nexusRepositoryId: 'test-nexus', packages: [[$class: 'MavenPackage', mavenAssetList: [[classifier: '', extension: 'jar', filePath: 'build\\libs\\DevOpsUsach2020-0.0.1.jar']], mavenCoordinate: [artifactId: 'DevOpsUsach2020', groupId: 'com.devopsusach2020', packaging: 'jar', version: '0.0.1']]]
                 }
-}*/
-return this;
+}
+return this;*/
