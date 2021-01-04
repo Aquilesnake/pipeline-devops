996e08ac6ce45e7e87ea1ab44288a608f6d32091
Cristian Tapia <criss.aquiles@gmail.com>
2021-01-04 15:07:53 -0300

    fix code 1  ejecucion.groovy v6

--- vars/gradle.groovy
+++ vars/gradle.groovy
@@ -12,7 +12,7 @@ def call (String etapa){
         stage('build'){
             bat './gradlew clean build'
         }
-        }else if("build;sonar"){
+        }if else("build;sonar"){
             stage('sonar') {
             def scannerHome = tool 'sonar';
                 withSonarQubeEnv('sonar') {
@@ -20,17 +20,17 @@ def call (String etapa){
                 }
             }
         }
-            }else if("build;sonar:run"){
+            }if else ("build;sonar:run"){
                 stage('run') {
                     bat 'start gradlew bootRun &'
                     sleep 20
                 }
             }
-                }else if("build;sonar;run;test"){
+                }if else ("build;sonar;run;test"){
                 stage('test') {
                 bat 'curl -X GET http://localhost:8082/rest/mscovid/test?msg=testing'
                 }
-                    }else if("build;sonar;run;test;nexus"){
+                    }if else ("build;sonar;run;test;nexus"){
                     stage('nexus') {
                     nexusPublisher nexusInstanceId: 'nexus', nexusRepositoryId: 'test-nexus', packages: [[$class: 'MavenPackage', mavenAssetList: [[classifier: '', extension: 'jar', filePath: 'build\\libs\\DevOpsUsach2020-0.0.1.jar']], mavenCoordinate: [artifactId: 'DevOpsUsach2020', groupId: 'com.devopsusach2020', packaging: 'jar', version: '0.0.1']]]
                     }
