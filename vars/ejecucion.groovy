c55785981671e9f82679c8393817046b74f8300f
Cristian Tapia <criss.aquiles@gmail.com>
2021-01-06 19:57:28 -0300

    change code 1

--- vars/ejecucion.groovy
+++ vars/ejecucion.groovy
@@ -1,19 +1,75 @@
 import ejecucion.groovy.*
+
+def call()
+{
+    pipeline
+    {
+        agent any
+        parameters
+        {
+            choice(name: 'par_buildtool', choices: ['gradle','maven'], description: 'Opciones disponibles para construcción de la aplicación')
+        }
+        stages
+        {
+            stage('Pipeline')
+            {
+                steps
+                {
+                    script
+                    {
+                        if (params.par_buildtool == 'gradle')
+                        {
+                            gradle.call()
+                        }
+                        else
+                        {
+                            maven.call()
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+return this;
+/*
 def call(){
-   parameters{ choice(name: 'compilador', choices:['gradle','maven'], description:'compilador de construcion para aplicacion')
+    
+    parameters{ choice(name: 'compilador', choices:['gradle','maven'], description:'compilador de construcion para aplicacion')
             //   choice(name: 'sub_stage', choices:['build','build,test y run','fullbuild'], description:'Construccion por stages')}
             string(name:'etapa',defaultValue:'',description:'Seleccion de stage.Opciones para Gradle: Build; Sonar; Run; Test; Nexus; gitCreateRelease. Opciones para Maven: Compile; Unit; Jar; Sonar; Sonar; Test; gitCreateRelease')}
-  pipeline {
+ 
+        stages{
+            stage{
+                script{
+                    println 'herramienta de ejecucion gradle' + params.compilador
+
+                    if(params.compilador == 'gradle'){
+                        gradle "${params.etapa}"
+                    } else {
+                        maven "${params.etapa}"
+                    }
+            }
+        }
+ 
+ /* pipeline {
         agent any
         stages{
             stage('pipeline'){
                 steps{
                     script{
+                        if (env.BRANCH_NAME)
+                                    'buildAndTest','sonar','runJar','rest','nexusCI'
+                        else if('downloadNexus','runDownloadedJar','rest','nexusCD')
+                        if(feature or develop ==)Ejecutar(pipeline CI)
+                        release == Ejecutar(pipeline CD)
+
                         echo 'herramienta seleccionada: ' + params.compilador
                         /*"${params.compilador}".call()*/
                         /*def pipe = load "${params.compilador}.groovy"
                         pipe.call()*/
-                        if(params.compilador == 'gradle' && params.etapa == 'build'){
+             /*           if(params.compilador == 'gradle' && params.etapa == 'build'){
                              gradle "build"
                             
                             }else if(params.compilador == 'gradle' && params.etapa == 'build;sonar;run'){
--- vars/gradle.groovy
+++ vars/gradle.groovy
@@ -1,5 +1,81 @@
 
-def call (String etapa = ''){
+import pipeline.*
+
+def call()
+{
+	figlet 'Gradle'
+
+	def arr_stages_CI = ['buildAndTest','sonar','runJar','rest','nexusCI']
+	def arr_stages_CD = ['downloadNexus','runDownloadedJar','rest','nexusCD']
+
+	String rama = sh(returnStdout: true, script: 'git rev-parse --abbrev-ref HEAD').trim();
+	Boolean var_expresion = ((rama.indexOf('feature')>0) || (rama.indexOf('develop')>0));
+
+	if (var_expresion == true)
+	{
+		figlet 'Continuous Integration'
+		buildAndTest()
+		sonar()
+		runJar()
+		rest()
+		nexusCI()
+	}
+	else
+	{
+		figlet 'Continuous Deploy'
+		downloadNexus()
+		runDownloadedJar()
+		rest()
+		nexusCD()
+	}
+}
+
+def buildAndTest()
+{
+	sh "gradle clean build"
+}
+
+def sonar()
+{
+	def sonarhome = tool 'sonar'
+	sh "${sonarhome}/bin/sonar-scanner -Dsonar.projectKey=ejemplo-gradle -Dsonar.java.binaries=build"
+}
+
+def runJar()
+{
+	sh "nohup gradle bootRun &"
+}
+
+def rest()
+{
+	sleep 5
+	sh "curl -X GET http://localhost:8081/rest/mscovid/test?msg=testing"
+}
+
+def downloadNexus()
+{
+    sh 'curl -X GET -u admin:criveros http://localhost:8082/repository/test-nexus/com/devopsusach2020/DevOpsUsach2020/0.0.1/DevOpsUsach2020-0.0.1.jar -O'
+}
+
+def runDownloadedJar()
+{
+    sh "java -jar DevOpsUsach2020-0.0.1.jar &"
+    sh "sleep 5"
+}
+
+def nexusCI()
+{
+	nexusPublisher nexusInstanceId: 'nexus', nexusRepositoryId: 'test-nexus', packages: [[$class: 'MavenPackage', mavenAssetList: [[classifier: '', extension: 'jar', filePath: 'DevOpsUsach2020-0.0.1.jar']], mavenCoordinate: [artifactId: 'DevOpsUsach2020', groupId: 'com.devopsusach2020', packaging: 'jar', version: '3.0.1']]]
+}
+
+def nexusCD()
+{
+	nexusPublisher nexusInstanceId: 'nexus', nexusRepositoryId: 'test-nexus', packages: [[$class: 'MavenPackage', mavenAssetList: [[classifier: '', extension: 'jar', filePath: 'DevOpsUsach2020-0.0.1.jar']], mavenCoordinate: [artifactId: 'DevOpsUsach2020', groupId: 'com.devopsusach2020', packaging: 'jar', version: '3.1.1']]]
+}
+
+return this;
+
+/*def call (String etapa = ''){
 
 switch(etapa) {
     case 'build':
